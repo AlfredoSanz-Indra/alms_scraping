f892b820cfdbfe9f040269663684146ceca2cb2f
asanzma <alfredosanz.indra@gmail.com>
2019-12-23 18:04:07 +0100

    adding front stuff

--- mainWebApp.py
+++ mainWebApp.py
@@ -37,6 +37,10 @@ def get_api():
     return jsonify({'api-routes': api_routes})
 
 
+#*******************************************************
+##WEB REQUEST WITH FLASK
+#*******************************************************
+
 @app.route("/")
 def hello():
     return "Alfredo Tools."
@@ -46,7 +50,42 @@ def hello():
 
 @app.route("/vf/web/")
 def helloWeb():
-    return "Alfredo Web Tools"
+        
+    logging.info("Entering hello index")
+
+    errormessage = '0'
+    tem_values = {}
+    resultData = {}
+
+    try:
+        logging.debug(request.url_root)
+
+        #Call to Controller
+        try:
+            #controller = Mainpage_Controller()
+            #errormessage, resultData = controller.datosBasicosPortada(request, u'PORTADA')
+
+            resultData['fecha'] = "10-10-2019"
+            
+        except Exception as err:
+            logging.error(str(err))            
+            logging.exception("@Error")
+        #
+
+        tem_values = {'errormessage':  errormessage,
+                      'domain': request.url_root[:-1],#quita la barra final
+                      'fecha': resultData['fecha']
+                     }
+
+    except Exception as err:
+        logging.error(str(err))
+        logging.exception("@Error")
+        errormessage = '*Error Redireting index: ' + str(err)
+    #
+    
+    #logging.info('tem_values=' + repr(tem_values))
+    logging.info("redirecting hello web index")
+    return render_template('index.html', datas = tem_values)
 #fin helloWeb
 
 
